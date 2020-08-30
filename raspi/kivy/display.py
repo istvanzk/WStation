aa3644abb904514453c9709aba3869e4ba7b4242
Istvan Z. Kovacs <istvanzk@gmail.com>
2020-08-30 21:36:24 +0200

    Move properties to screen manager; read weather data from message queue

--- raspi/kivy/display.py
+++ raspi/kivy/display.py
@@ -18,6 +18,8 @@ Window.resizable = '0'
 Window.borderless = True
 #Window.fullscreen = True
 
+# Debug messages
+DEBUG_MSG = False
 
 # Message queue parameters
 RXQUEUE_NAME  = "/rf22b_server_tx"
@@ -45,18 +47,62 @@ class ClientMq():
 
         print("Message received: %s (%d) - %s\n" % (s, p, sd))
 
-    def read_msg(self):
-        s, p = self.mqRX.receive()
-        unpacked_s = struct.unpack('HHHHHHBBBBBb', s[:18])
-        #sd = s.decode()
-        print("Message#%d:(%d):" % (m, p))
-        print(unpacked_s)
-        print(":".join("{:02x}".format(ord(c)) for c in s[18:38].decode()))
+    def read_weather_data(self, timeout_sec=None):
+       
+        # The local dictionary for storing the most recent weather data
+        _weather_data = {}
 
+        # Receive and unpack message from the MessageQueue        
+        msg, pri = self.mqRX.receive(timeout=timeout_sec)
+        #msg = b''
+        unpacked_msgheader = struct.unpack('HHHHHHBBBBBb', msg[:18])
+        
+        _weather_data['Header'] = unpacked_msgheader
 
-class FirstScreen(Screen):
-    wind_direction = NumericProperty(0.0)
-    wind_speed = NumericProperty(0.5)
+        # Show raw data
+        #print(F"Message ({p:d}):")
+        #print(unpacked_msgheader)
+        #print(":".join("{:1s}".format(chr(c)) for c in msg[18:38]))
+
+        # Decode the 20 bytes weather data (from the RF22B/Arduino)
+        ii = 18
+        lng = 0
+        while ii < 38:
+            if chr(msg[ii]) == 'N':
+                val = msg[ii+1]
+                lng = 1
+
+                #print(F"{chr(msg[ii]):1s}: {val:1d}")
+               
+            elif chr(msg[ii]) in ['S', 'T', 'P', 'H']:
+                if chr(msg[ii]) is 'P':
+                    val_d = bytearray(msg[ii+1:ii+6])
+                    lng = 5
+                else:
+                    val_d = bytearray(msg[ii+1:ii+4])                    
+                    lng = 3
+
+                if val_d[0] == 0x20:
+                    val_d[0] = 0x30
+
+                val = 0
+                for dd in range(lng):
+                    val += (val_d[lng-dd-1]-0x30)*10**(dd-1)   
+
+                #print(F"{chr(msg[ii]):1s}: {val:.1f}")
+
+            _weather_data[chr(msg[ii])] = val
+
+            ii += (lng+1)
+
+        return _weather_data
+
+class MyScreenManager(ScreenManager):    
+
+    # Displayed info and their color
+    #(access with root.manager.* from the kv file)
+    _wind_direction = NumericProperty(1)
+    _wind_speed = NumericProperty(0.5)
     # HSV: Hue = degrees/360
     #     Red: 0 and 60 degrees.
     #     Yellow: 61 and 120 degrees.
@@ -64,57 +110,84 @@ class FirstScreen(Screen):
     #     Cyan: 181 and 240 degrees.
     #     Blue: 241 and 300 degrees.
     #     Magenta: 301 and 360 degrees.
-    wind_speed_hue = NumericProperty(160)
-    air_temperature = NumericProperty(20.5)
-    air_temperature_color = ListProperty([0,1,0])
-    air_pressure = NumericProperty(1008.5)
-    air_relhumidity = NumericProperty(61.5)
+    _wind_speed_hue = NumericProperty(160)
+    _air_temperature = NumericProperty(23.5)
+    _air_temperature_color = ListProperty([0,1,0])
+    _air_pressure = NumericProperty(1000.8)
+    _air_relhumidity = NumericProperty(64.5)
 
-    _update_display_ev = None
+    # The dictionary storing the most recent weather data read from the message queue
+    weather_data = {'N': _wind_direction,'S':_wind_speed, 'T':_air_temperature, 'P':_air_pressure,'H':_air_relhumidity}
+
+    # How to update the values
+    _update_display_rnd  = True
+    _update_display_msgq = False
+
+    if _update_display_msgq:
+        msg_queue = ClientMq()
 
     def __init__(self, **kwargs):
-        super(FirstScreen, self).__init__(**kwargs)
-        Clock.schedule_interval(self.update_display_info, 1)
+        super(MyScreenManager, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update_display_info, 1.0)
 
     def update_display_info(self, *args):
-        if random() > 0.5:
-            self.air_temperature += 2*random()
-        else:
-            self.air_temperature -= 2*random()
 
-        if self.air_temperature < -10:
-            self.air_temperature_color = [0,0,1]
-        elif self.air_temperature < 0 and self.air_temperature > -10:
-            self.air_temperature_color = [0,0,0.5]
-        elif self.air_temperature > 0 and self.air_temperature < 20:
-            self.air_temperature_color = [0,1,0]
-        elif self.air_temperature > 20 and self.air_temperature < 35:
-            self.air_temperature_color = [0.5,0,0]
-        elif self.air_temperature > 35:
-            self.air_temperature_color = [1,0,0]
+        # Update the values
+        # Random: display testing
+        # MsgQueue: normal operation
+        if self._update_display_rnd:
+            if random() > 0.5:
+                self._air_temperature += 2*random()
+            else:
+                self._air_temperature -= 2*random()
 
-        if random() > 0.5:
-            self.air_pressure += 5*random()
-        else:
-            self.air_pressure -= 5*random()
+            if random() > 0.5:
+                self._air_pressure += 5*random()
+            else:
+                self._air_pressure -= 5*random()
 
-        if random() > 0.5:
-            self.air_relhumidity += random()
-        else:
-            self.air_relhumidity -= random()
+            if random() > 0.5:
+                self._air_relhumidity += random()
+            else:
+                self._air_relhumidity -= random()
 
-        if random() > 0.5:
-            self.wind_speed = 35*random()
-        self.wind_speed_hue = (160-205*log10(1+self.wind_speed/5))/360
+            if random() > 0.5:
+                self._wind_speed = 35*random()
 
-        if random() > 0.5:
-            self.wind_direction= 360*random()
+            if random() > 0.5:
+                self._wind_direction= 360*random()
+        
+        elif self._update_display_msgq:
+            self.weather_data = self.msg_queue.read_weather_data(1.0)
+            # TODO: adjust North direction based on calibration data
+            self._wind_direction  = self.weather_data['N']*22.5
+            self._air_temperature = self.weather_data['T']
+            self._wind_speed      = self.weather_data['S']
+            self._air_pressure    = self.weather_data['P']
+            self._air_relhumidity = self.weather_data['H']
+
+
+        # Set the info display colors depending on their value/range
+        if self._air_temperature < -10:
+            self._air_temperature_color = [0,0,1]
+        elif self._air_temperature < 0 and self._air_temperature > -10:
+            self._air_temperature_color = [0,0.5,0.5]
+        elif self._air_temperature > 0 and self._air_temperature < 20:
+            self._air_temperature_color = [0,1,0]
+        elif self._air_temperature > 20 and self._air_temperature < 35:
+            self._air_temperature_color = [0.5,0.5,0]
+        elif self._air_temperature > 35:
+            self._air_temperature_color = [1,0,0]
+
+        self._wind_speed_hue = (160-205*log10(1+self._wind_speed/5))/360
+
+
+class FirstScreen(Screen):
+    pass
 
 class SecondScreen(Screen):
     pass
 
-class MyScreenManager(ScreenManager):    
-    pass
 
 root_widget = Builder.load_file('screens.kv')
 
